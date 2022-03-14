de598e0589ab75e0a0e77dddfafa50ddbddb6256
RetepMil <lkslyj2@naver.com>
2022-03-14 17:11:27 +0900

    1.5 styled jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import styles from "./NavBar.module.css";
 
 function NavBar() {
+  const router = useRouter();
+  console.log(router);
   return (
     <nav>
       <Link href="/">
         <a>Home</a>
       </Link>
-      <Link href="/about/">
+      <Link href="/about">
         <a>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+
+        a {
+          text-decoration: none;
+        }
+      `}</style>
     </nav>
   );
 }
