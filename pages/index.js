d1bcac52a1499a063c0bc063833627c2eb8fa8fa
RetepMil <lkslyj2@naver.com>
2022-03-15 16:30:07 +0900

    2.1 tmdb api implementation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
 import HeadTitle from "../components/HeadTitle";
 
-export default function index() {
+const API_KEY = "db271945223b41a08e41040cf80dcfa2";
+
+export default function Index() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      console.log(results);
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <HeadTitle title="Home" />
-      <h1>asdf</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
